8d87b7672b9ca43f7eb9f87f222310f77ddc7bb5
nguyenlthoe <nguyenlthoe@gmail.com>
2022-08-21 23:16:14 +0700

    fix

--- kafkatask/src/main/java/todo/Main.java
+++ kafkatask/src/main/java/todo/Main.java
@@ -19,14 +19,23 @@ public final class Main {
      * @param args
      */
     public static void main(final String[] args) {
+        final long timeUpdate = 60000;
         SparkSession spark = SparkSession.builder()
                 .appName("Query hdfs")
                 .master("yarn")
                 .getOrCreate();
-        ReadHdfs read = new ReadHdfs(spark);
-        Dataset<Row> df = read.read();
-        QueryDataframe queryDf = new QueryDataframe();
-        queryDf.countClickAndView(df);
+        while (true) {
+            ReadHdfs read = new ReadHdfs(spark);
+            Dataset<Row> df = read.read();
+            QueryDataframe queryDf = new QueryDataframe();
+            queryDf.countClickAndView(df);
+            try {
+                Thread.sleep(timeUpdate);
+            } catch (InterruptedException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            }
+        }
     }
 
 }
